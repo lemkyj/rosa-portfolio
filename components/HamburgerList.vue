d33fdf88c09ebd639739610bd52e36611bf47ec6
lemkyj <lemkyj@gmail.com>
2021-03-21 17:48:18 -0400

    burger list updated

---
+++ components/HamburgerList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="fixed inset-0 bg-brand-pink w-full h-full z-20">
+    <div class="flex justify-end mt-8 mr-8">
+      <svg
+        width="46"
+        height="46"
+        viewBox="0 0 25 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="Hamburger-svg">
+          <rect
+            id="line-top"
+            x="3.32892"
+            width="30.547"
+            height="3.39411"
+            rx="1.69706"
+            transform="rotate(45 3.32892 0)"
+            fill="#4A4A48"
+          />
+          <rect
+            id="line-mid"
+            x="11.2319"
+            y="10.1823"
+            width="3.39411"
+            height="3.39411"
+            rx="1.69706"
+            fill="#4A4A48"
+          />
+          <rect
+            id="line-bottom"
+            x="0.928925"
+            y="21.6"
+            width="30.547"
+            height="3.39411"
+            rx="1.69706"
+            transform="rotate(-45 0.928925 21.6)"
+            fill="#4A4A48"
+          />
+        </g>
+      </svg>
+    </div>
+    <div
+      class="flex flex-col w-full h-full justify-center items-center space-y-14"
+    >
+      <nuxt-link exact to="/" class="text-5xl font-bold">Home</nuxt-link>
+      <nuxt-link to="/portfolio" class="text-5xl font-bold"
+        >Portfolio</nuxt-link
+      >
+      <a href="mailto:lia8488@hotmail.com" class="text-5xl font-bold">Email</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
