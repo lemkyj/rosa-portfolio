b18660519bbfe139ff2c2f23e7f603287c15b31c
lemkyj <lemkyj@gmail.com>
2021-03-22 08:02:32 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: "description",
+        hid: "Rosa Lee",
         name: "Rosa Lee",
         content:
           process.env.npm_package_description ||
